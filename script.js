ad7c371c5dda83da7193cfa6e8a7992181c78db6
Vallyn2096 <fressyfirmansyah@gmail.com>
2025-09-25 11:48:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar berubah warna saat scroll
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
